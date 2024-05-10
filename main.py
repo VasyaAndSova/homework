e0bef33f305238d2d4e3832101a129d2c8a65832
Василиса Андросова <and3vasya@yandex.ru>
2024-05-10 11:52:26 +0300

    end of work

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from src.widget import get_mask_card_or_account
+from src.widget import get_mask_card_or_account, get_date_string
 
 print(get_mask_card_or_account("Maestro 1596837868705199"))
+print(get_mask_card_or_account("Счет 64686473678894779589"))
+print(get_date_string('2018-07-11T02:26:18.671407'))
+
