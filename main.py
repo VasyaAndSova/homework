1aaaa96e6617f87130da5c57bc29b95da88a8d2d
Василиса Андросова <and3vasya@yandex.ru>
2024-05-10 12:01:35 +0300

    all lints checked

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from src.widget import get_mask_card_or_account, get_date_string
+from src.widget import get_date_string, get_mask_card_or_account
 
 print(get_mask_card_or_account("Maestro 1596837868705199"))
 print(get_mask_card_or_account("Счет 64686473678894779589"))
-print(get_date_string('2018-07-11T02:26:18.671407'))
-
+print(get_date_string("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,19 @@
-from src.masks import get_mask_card, get_mask_account
+from src.masks import get_mask_account, get_mask_card
+
+
 def get_mask_card_or_account(numbers: str) -> str:
     """Функция принимает тип карты/счета и номер карты/счета и возращает их маску"""
-    if 'Счет' in numbers:
+    if "Счет" in numbers:
         mask_account = get_mask_account(numbers.split()[-1])
-        result = f'{numbers[:-20]}{mask_account}'
+        result = f"{numbers[:-20]}{mask_account}"
         return result
     else:
         mask_card = get_mask_card(numbers.split()[-1])
-        result = f'{numbers[:-16]}{mask_card}'
+        result = f"{numbers[:-16]}{mask_card}"
         return result
 
 
 def get_date_string(date: str) -> str:
     """Функция принимает строку и возвращает строку с датой"""
-    date_string = date[:10].split('-')[::-1]
-    return '.'.join(date_string)
-
-
-
+    date_string = date[:10].split("-")[::-1]
+    return ".".join(date_string)
