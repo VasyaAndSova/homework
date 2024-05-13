d4650f28287d26433f3dea0826f74c4c8206295b
Василиса Андросова <and3vasya@yandex.ru>
2024-05-13 14:32:56 +0300

    add second def in processing.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.processing import get_new_list_dict
+from src.processing import get_dict_date_dicrease, get_new_list_dict
 from src.widget import get_date_string, get_mask_card_or_account
 
 print(get_mask_card_or_account("Maestro 1596837868705199"))
@@ -24,5 +24,27 @@ print(
             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         ],
         "CANCELED",
+    )
+)
+
+print(
+    get_dict_date_dicrease(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+print(
+    get_dict_date_dicrease(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        False,
     )
 )
--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,20 @@
+from typing import Any
+
+
 def get_new_list_dict(list_dict: list[dict[str, object]], key_state: str = "EXECUTED") -> list[dict[str, object]]:
+    """Функция возвращает список тех словарей, у которых ключ state содержит переданное в функцию значение"""
     new_list_dict = []
     for dicts in list_dict:
         if dicts["state"] == key_state:
             new_list_dict.append(dicts)
     return new_list_dict
+
+
+def get_dict_date_dicrease(list_dict: Any, sorting_order: bool = True) -> list[dict[str, object]]:
+    """Функция возвращает список словарей отсортированных по убыванию даты"""
+    if sorting_order is True:
+        new_list_dict = sorted(list_dict, key=lambda dicts: dicts["date"], reverse=True)
+        return new_list_dict
+    else:
+        new_list_dict = sorted(list_dict, key=lambda dicts: dicts["date"])
+        return new_list_dict
