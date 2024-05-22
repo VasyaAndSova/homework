5e34a63e791d14734b59fe9841ffe3ac03023108
Василиса Андросова <and3vasya@yandex.ru>
2024-05-22 13:01:54 +0300

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -13,7 +13,7 @@ def get_new_list_dict(list_dict: list[dict[str, object]], key_state: str = "EXEC
 def get_dict_date_dicrease(list_dict: Any, sorting_order: bool = True) -> list[dict[str, object]]:
     """Функция возвращает список словарей отсортированных по убыванию даты"""
     if sorting_order is True:
-        new_list_dict = sorted(list_dict, key=lambda dicts: dicts["date"], reverse=True)
+        new_list_dict = sorted(list_dict, key=lambda dicts: dicts["date"], reverse=sorting_order)
         return new_list_dict
     else:
         new_list_dict = sorted(list_dict, key=lambda dicts: dicts["date"])
